aa88f2ce1f4b81e41451d1a34e1f54129458b552
guaddha <arismendig9@gmail.com>
2022-11-16 00:12:49 -0300

    suma de num1 y num2

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,14 @@ console.log(nombre);
 console.log(nombre2);
 console.log(dni);
 //#endregion nombres
+
+//#region suma
+let num1 = 60;
+let num2 = 60;
+function suma(num1, num2) {
+    let k = num1 + num2;
+    return k;
+}
+console.log(suma(num1, num2));
+//#endregion suma
+
