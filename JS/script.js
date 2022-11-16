ce8f7a9b8e7024d7f82b823734631e6ce6cfe13a
guaddha <arismendig9@gmail.com>
2022-11-16 00:11:21 -0300

    console.log

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,10 @@ const nombre = `Guada`;
 let nombre2 = "Guadalupe";
 var dni = '46330055';
 //#endregion variables
+
+
+//#region nombres
+console.log(nombre);
+console.log(nombre2);
+console.log(dni);
+//#endregion nombres
